c88fc1815947158d1f1fa3e22224a51afe6a6069
Brandon Cohen <brandon.f.cohen@gmail.com>
2021-11-25 10:33:22 -0500

    esri layer working

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    with open('leafletPrep.json', 'r') as file:
-        tiles=json.loads(file.read())
+    with open('latest_overlap/nsm_latest.json', 'r') as file:
+        layers=json.loads(file.read())
 
-    return render_template('leafletkmz.html', tiles = tiles)
+    return render_template('leafletkmz.html', layers = layers)
+
+
+@app.route("/leaflet")
+def leaflet():
+    return(render_template('leaflet.html'))
